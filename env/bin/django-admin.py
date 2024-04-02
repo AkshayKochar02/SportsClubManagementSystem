d6c9f35baf61708c1ca9ab1412bafb2e400ff447
EC2 Default User <ec2-user@ip-172-31-41-218.eu-west-1.compute.internal>
2024-04-02 13:15:36 +0000

    Django version change

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ec2-user/environment/Sports_Club_Management_System_in_Django/sportsClubManagement-django/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
