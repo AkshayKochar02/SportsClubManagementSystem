fddef20e762668d9433f54c02d0e5f0fd3a46332
EC2 Default User <ec2-user@ip-172-31-41-218.eu-west-1.compute.internal>
2024-04-04 21:06:04 +0000

    more changes

--- scmapp/models.py
+++ scmapp/models.py
@@ -2,28 +2,28 @@
 from django.db import models
 # from django.contrib.auth.models import User
 
-"""Models creation """
-class User(models.Model):
+
+class User(models.Model): #User class members
     uid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=40)
     gender = models.CharField(max_length=6)
     password = models.CharField(max_length=20)
 
-class Admin(models.Model):
+class Admin(models.Model): #Admin class members
     aid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=40)
     password = models.CharField(max_length=20)
 
-class Event(models.Model):
+class Event(models.Model): #Event class members
     eid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     date = models.DateField()
     time = models.TimeField()
     duration = models.IntegerField()
 
-class Book_ground(models.Model):
+class Book_ground(models.Model): #Book_ground class members
     bid = models.AutoField(primary_key=True)
     uid = models.IntegerField()
     name = models.CharField(max_length=20)
--- scmapp/tests.py
+++ scmapp/tests.py
@@ -1,4 +1,2 @@
 """Tests python file docstring """
-from django.test import TestCase
 
-# Create your tests here.
