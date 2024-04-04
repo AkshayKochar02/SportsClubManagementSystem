c421217a3dd6548a7af02e4ceddef33e1498e820
EC2 Default User <ec2-user@ip-172-31-41-218.eu-west-1.compute.internal>
2024-04-04 21:14:35 +0000

    bit more changes

--- scmapp/models.py
+++ scmapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # from django.contrib.auth.models import User
 
 
-class User(models.Model): 
+class User(models.Model):
     #User class members
     uid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
@@ -11,14 +11,14 @@ class User(models.Model):
     gender = models.CharField(max_length=6)
     password = models.CharField(max_length=20)
 
-class Admin(models.Model): 
+class Admin(models.Model):
     #Admin class members
     aid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
     email = models.CharField(max_length=40)
     password = models.CharField(max_length=20)
 
-class Event(models.Model): 
+class Event(models.Model):
     #Event class members
     eid = models.AutoField(primary_key=True)
     name = models.CharField(max_length=20)
@@ -26,7 +26,7 @@ class Event(models.Model):
     time = models.TimeField()
     duration = models.IntegerField()
 
-class Book_ground(models.Model): 
+class Book_ground(models.Model):
     #Book_ground class members
     bid = models.AutoField(primary_key=True)
     uid = models.IntegerField()
